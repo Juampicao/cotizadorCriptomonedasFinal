ccd3f5ab2275761f2fb8cf30e31ccd65c4bd0043
juampicao <juanpiparada@gmail.com>
2022-05-20 12:04:40 -0300

    change banner photo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useState, useEffect } from 'react'
-import styled from '@emotion/styled'
-import Formulario from './components/Formulario'
-import Resultado from './components/Resultado'
-import Spinner from './components/Spinner'
-import ImagenCripto from './img/imagen-criptos.png'
+import { useState, useEffect } from "react";
+import styled from "@emotion/styled";
+import Formulario from "./components/Formulario";
+import Resultado from "./components/Resultado";
+import Spinner from "./components/Spinner";
+import ImagenCripto from "./img/imagen-criptos.png";
+import ImagenCripto2 from "./img/imagencripto2.webp";
 
 const Contenedor = styled.div`
   max-width: 900px;
@@ -14,17 +15,17 @@ const Contenedor = styled.div`
     grid-template-columns: repeat(2, 1fr);
     column-gap: 2rem;
   }
-`
+`;
 const Imagen = styled.img`
   max-width: 400px;
   width: 80%;
   margin: 100px auto 0 auto;
   display: block;
-`
+`;
 
 const Heading = styled.h1`
-  font-family: 'Lato', sans-serif;
-  color: #FFF;
+  font-family: "Lato", sans-serif;
+  color: #fff;
   text-align: center;
   font-weight: 700;
   margin-top: 80px;
@@ -32,62 +33,54 @@ const Heading = styled.h1`
   font-size: 34px;
 
   &::after {
-    content: '';
+    content: "";
     width: 100px;
     height: 6px;
-    background-color: #66A2FE;
+    background-color: #66a2fe;
     display: block;
     margin: 10px auto 0 auto;
   }
-` 
+`;
 
 function App() {
-
-  const [ monedas, setMonedas ] = useState({})
-  const [ resultado, setResultado ] = useState({})
-  const [ cargando, setCargando ] = useState(false)
+  const [monedas, setMonedas] = useState({});
+  const [resultado, setResultado] = useState({});
+  const [cargando, setCargando] = useState(false);
 
   useEffect(() => {
-      if(Object.keys(monedas).length > 0) {
-          
-        const cotizarCripto = async () => {
-            setCargando(true)
-            setResultado({})
+    if (Object.keys(monedas).length > 0) {
+      const cotizarCripto = async () => {
+        setCargando(true);
+        setResultado({});
 
-            const { moneda, criptomoneda } = monedas
-            const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`
+        const { moneda, criptomoneda } = monedas;
+        const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
 
-            const respuesta = await fetch(url)
-            const resultado = await respuesta.json()
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
 
-            setResultado(resultado.DISPLAY[criptomoneda][moneda])
+        setResultado(resultado.DISPLAY[criptomoneda][moneda]);
 
-            setCargando(false)
-        }
+        setCargando(false);
+      };
 
-        cotizarCripto()
-      }
-  }, [monedas])
+      cotizarCripto();
+    }
+  }, [monedas]);
 
   return (
-      <Contenedor>
-          <Imagen 
-            src={ImagenCripto}
-            alt="imagenes criptomonedas"
-          />
+    <Contenedor>
+      <Imagen src={ImagenCripto2} alt="imagenes criptomonedas" />
 
-          <div>
-              <Heading>Cotiza Criptomonedas al Instante</Heading>
-              <Formulario 
-                setMonedas={setMonedas}
-              />
+      <div>
+        <Heading>Cotiza Criptomonedas al Instante</Heading>
+        <Formulario setMonedas={setMonedas} />
 
-              {cargando && <Spinner />}
-              {resultado.PRICE && <Resultado resultado={resultado} />} 
-          </div>
-
-      </Contenedor>
-  )
+        {cargando && <Spinner />}
+        {resultado.PRICE && <Resultado resultado={resultado} />}
+      </div>
+    </Contenedor>
+  );
 }
 
-export default App
+export default App;
